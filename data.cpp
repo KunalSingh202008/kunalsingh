9b68804e04a0a911259eed5b1986198ef1e91a4e
Kunal Singh <kunalsingh20.2k25@gmail.com>
2025-12-19 11:55:34 +0530

    Myt first

---
+++ data.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fib(int a)
+{
+    if(a==1)
+    return 0;
+    else if (a==2)
+    return 1;
+    else 
+    return(fib(a-1)+fib(a-2));
+}
+int main()
+{
+    int n,x;
+    
+    cin>>n;
+    x=fib(n);
+    cout<<x;
+    
+	
+
+}
